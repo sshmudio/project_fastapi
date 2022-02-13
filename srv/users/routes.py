21fdb79eb21d1da8fdaaa5dd7b02c5a3dfcdcc9c
Sshmudio <sshmudio@gmail.com>
2022-02-13 22:39:46 +0200

    Add class Cruder

--- srv/users/routes.py
+++ srv/users/routes.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
-from srv.utils import services
+from srv.utils.services import Cruder
 from srv.users.schemas import UserSchema, UserDb
 from srv.utils.dbutils import get_db
 
@@ -11,13 +11,13 @@ api = APIRouter()
 
 @api.post("/user/", response_model=UserDb, status_code=201)
 async def create_user(*, db: Session = Depends(get_db), payload: UserSchema):
-    user = services.post(db_session=db, payload=payload)
+    user = Cruder.post(db_session=db, payload=payload)
     return user
 
 
 @api.get("/user/{id}/", response_model=UserDb)
 async def get_user(*, db: Session = Depends(get_db), id: int = Path(..., gt=0)):
-    user = services.get(db_session=db, id=id)
+    user = Cruder.get(db_session=db, id=id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
@@ -25,23 +25,23 @@ async def get_user(*, db: Session = Depends(get_db), id: int = Path(..., gt=0)):
 
 @api.get("/user-list/", response_model=List[UserDb])
 async def get_users(db: Session = Depends(get_db)):
-    return services.get_all(db_session=db)
+    return Cruder.get_all(db_session=db)
 
 
 @api.put("/user/{id}/", response_model=UserDb)
 async def update_user(*, db: Session = Depends(get_db), id: int = Path(..., gt=0), payload: UserSchema):
-    user = services.get(db_session=db, id=id)
+    user = Cruder.get(db_session=db, id=id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    user = services.put(
+    user = Cruder.put(
         db_session=db, user=user, username=payload.username, email=payload.email, password=payload.password)
     return user
 
 
 @api.delete("/user/{id}/", response_model=UserDb)
 async def delete_user(*, db: Session = Depends(get_db), id: int = Path(..., gt=0)):
-    user = services.get(db_session=db, id=id)
+    user = Cruder.get(db_session=db, id=id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    user = services.delete(db_session=db, id=id)
+    user = Cruder.delete(db_session=db, id=id)
     return user
--- srv/utils/services.py
+++ srv/utils/services.py
@@ -4,32 +4,59 @@ from srv.users.schemas import UserSchema
 from srv.utils.passwdhash import create_password_hash
 
 
-def post(db_session: Session, payload: UserSchema):
-    user = User(username=payload.username, email=payload.email, password=create_password_hash(payload.password))
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
-    return user
+class Cruder:
+    def post(db_session: Session, payload: UserSchema):
+        user = User(username=payload.username, email=payload.email, password=create_password_hash(payload.password))
+        db_session.add(user)
+        db_session.commit()
+        db_session.refresh(user)
+        return user
+
+    def get(db_session: Session, id: int):
+        return db_session.query(User).filter(User.id == id).first()
+
+    def get_all(db_session: Session):
+        return db_session.query(User).all()
+
+    def put(db_session: Session, user: User, username: str, email: str, password: str):
+        user.username = username
+        user.email = email
+        user.password = create_password_hash(password)
+        db_session.commit()
+        return user
+
+    def delete(db_session: Session, id: int):
+        user = db_session.query(User).filter(User.id == id).first()
+        db_session.delete(user)
+        db_session.commit()
+        return user
+
+# def post(db_session: Session, payload: UserSchema):
+#     user = User(username=payload.username, email=payload.email, password=create_password_hash(payload.password))
+#     db_session.add(user)
+#     db_session.commit()
+#     db_session.refresh(user)
+#     return user
 
 
-def get(db_session: Session, id: int):
-    return db_session.query(User).filter(User.id == id).first()
+# def get(db_session: Session, id: int):
+#     return db_session.query(User).filter(User.id == id).first()
 
 
-def get_all(db_session: Session):
-    return db_session.query(User).all()
+# def get_all(db_session: Session):
+#     return db_session.query(User).all()
 
 
-def put(db_session: Session, user: User, username: str, email: str, password: str):
-    user.username = username
-    user.email = email
-    user.password = create_password_hash(password)
-    db_session.commit()
-    return user
+# def put(db_session: Session, user: User, username: str, email: str, password: str):
+#     user.username = username
+#     user.email = email
+#     user.password = create_password_hash(password)
+#     db_session.commit()
+#     return user
 
 
-def delete(db_session: Session, id: int):
-    user = db_session.query(User).filter(User.id == id).first()
-    db_session.delete(user)
-    db_session.commit()
-    return user
+# def delete(db_session: Session, id: int):
+#     user = db_session.query(User).filter(User.id == id).first()
+#     db_session.delete(user)
+#     db_session.commit()
+#     return user
